1f861932f1668450807ee4039e346f668ad85988
surya-tn99 <balasurya.tn99@gmail.com>
2025-12-22 22:34:17 +0530

    modified the way we access  mongo db

--- utils/mongo.js
+++ utils/mongo.js
@@ -2,13 +2,24 @@ const mongo = require("mongodb");
 
 const mongoClinet = mongo.MongoClient;
 
+let _db;
+
 exports.connectMongoDB = callback => {
     mongoClinet.connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.10")
         .then(client => {
             console.log("mongodb connected");
+            const dataName = "shop";
+            _db = client.db(dataName);
             callback(client);
         })
         .catch(err => {
             console.error(err);
         })
+}
+
+exports.getDb = ()=>{
+    if(_db){
+        return _db;
+    }
+    throw "no database found";
 }
